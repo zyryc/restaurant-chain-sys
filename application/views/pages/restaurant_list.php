67b7b35e5309d93054f6188893659179d20351ef
unknown <danruiyot@gmail.com>
2022-06-20 20:20:38 +0300

    changes

--- application/views/pages/restaurant_list.php
+++ application/views/pages/restaurant_list.php
@@ -1,10 +1,8 @@
 <br>
 <div class="container">
-<div class="row">
-<div class="col-sm-3">
-&nbsp;
-</div>
-<div class="col-sm-6 border bg-light">
+<div class="">
+
+<div class="border bg-light p-2">
 <br>
 <?php if ($this->session->flashdata('success')): ?>                 
             <div class="alert alert-success"><?php echo $this->session->flashdata('success'); ?></div>
@@ -14,37 +12,31 @@
 
 <?php if(count($restaurants)){ $count = 0;?>
 	<h2 class="text-center"><?php echo $title; ?></h2>
-	<div class="table" style="max-width:600px">
-		<table class="table  table-bordered">
-			<thead class="thead-dark">
-			<tr >
-				<th>#</th>
-				<th>Name</th>
-				<th>Logo</th>
-				<th>Location</th>
-			</tr>
-			</thead>
+		<div class="row">
+			
 			<?php foreach ($restaurants as $item): ?>
 
-			<tbody>
-			<tr onclick="window.location='<?php echo base_url() . "home/home/" .$item['slug']; ?>'">
-			<td><?php echo $count + 1; ?></td>
 
-				<td><?php echo $item['name']; ?></td>
-				<td><img src="<?php echo base_url().'assets/images/'.$item['logo'];?>" class="img-thumbnail" alt="<?php echo $item['name']; ?>" width="20%">
-				</td>
-				<td><?php echo $item['location']; ?></td>
+			<div class="col-md-3">
+			<div class="card">
+			<div onclick="window.location='<?php echo base_url() . "home/home/" .$item['slug']; ?>'">
+				<img src="<?php echo base_url().'assets/images/'.$item['logo'];?>" class="card-img-top" alt="<?php echo $item['name']; ?>" width="20%">
+			</div>
+			<div class="card-body">
+				<h5 class="card-title"><?php echo $item['name']; ?></h5>
+				<span><i class="fa fa-map-marker w3-red">&nbsp;</i><?php echo $item['location']; ?></span>
 				
-			</tr>
-			</tbody>
+			</div>
+			</div>
+			</div>
 			<?php  $count++;  endforeach; ?>
-		</table>
-	</div>
+		</div>
+
 	<?php } else { ?>
     <h4>No Restaurant added yet, click here to add <a class="btn-success btn" href="<?php echo base_url(); ?>restaurant/create/">Create</a></h4>            
   <?php } ?>
  
-
+<br>
 </div>
 </div>
 </div>
